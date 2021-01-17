afa135c09a799454c5ad2ce7519480ba2a2b09ba
Burak Tunc <burak.tunc@check24.de>
2021-01-17 21:32:43 +0100

    Initial Commit

---
+++ SwiftUICustomTabbar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICustomTabbar
+//
+//  Created by Burak Tunc on 17.01.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUICustomTabbar/SwiftUICustomTabbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICustomTabbarApp.swift
+//  SwiftUICustomTabbar
+//
+//  Created by Burak Tunc on 17.01.21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICustomTabbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
